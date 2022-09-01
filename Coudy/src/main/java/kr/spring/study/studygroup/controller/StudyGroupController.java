9acca6e43fdb99fec8167de15c45af617ff2679f
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2022-09-01 23:50:39 +0900

    clean up

--- Coudy/src/main/java/kr/spring/study/studygroup/controller/StudyGroupController.java
+++ Coudy/src/main/java/kr/spring/study/studygroup/controller/StudyGroupController.java
@@ -6,11 +6,8 @@ import org.mybatis.logging.Logger;
 import org.mybatis.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudyGroupController {
--- Coudy/src/main/java/kr/spring/study/studygroup/controller/StudyGroupMainController.java
+++
@@ -1,7 +0,0 @@
-package kr.spring.study.studygroup.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class StudyGroupMainController {
-}
