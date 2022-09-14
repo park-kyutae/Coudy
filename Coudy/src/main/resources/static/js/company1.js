11c4c858af3f3be5269a1c5785a7b52bf0be04cc
xoals6536 <xoals6536@naver.com>
2022-09-14 21:42:48 +0900

    js작업중6666 home에선 완료

--- Coudy/src/main/resources/static/js/company1.js
+++ Coudy/src/main/resources/static/js/company1.js
@@ -1,10 +1,9 @@
 const scrap = {
     init: function() {
-        $(document).on('click', '.scrap_star', function() {
+        $(document).on('click', '.scrap_star .scrap_btn', function() {
             let com_num = $(this).data('com-num');
 
             console.log(com_num);
-            if(confirm('스크랩 하시겠습니까?')){
                 $.ajax({
                     url:'scrap.do',
                     type:'post',
@@ -19,7 +18,10 @@ const scrap = {
                             alert('로그인 후 이용가능');
                         }else if(param.result=='success'){
                             console.log(param);
-                            displayScrap(param);
+                            //displayScrap(param);
+                            //window.location.reload();
+                            location.reload();
+                            //history.go(0);
                         }else{
                             alert('스크랩 중 오류');
                         }
@@ -28,9 +30,7 @@ const scrap = {
                         alert('네트워크오류발생');
                     }
                 });
-            }else {
-                return false;
-            }
+
         });
         // $(document).on('click','.unscrap_star',function () {
         //     $(this).children().attr('d','M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z')
@@ -40,40 +40,40 @@ const scrap = {
         // })
     }
 }
-const scrap_btn = {
-    init: function() {
-        $(document).on('click', '.scrap_btn', function() {
-            alert('엥 못읽냐?');
-            if(confirm('스크랩 하시겠습니까?')){
-                console.log('scrap');
-                $(this).children().children().attr('d','M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z')
-                $(this).removeClass('scrap_btn');
-                $(this).addClass('unscrap_btn');
-            }else {
-                return false;
-            }
-        });
-        $(document).on('click','.unscrap_btn',function () {
-            $(this).children().children().attr('d','M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z')
-            console.log('unscrap');
-            $(this).removeClass('unscrap_btn');
-            $(this).addClass('scrap_btn');
-        })
-    }
-}
+// const scrap_btn = {
+//     init: function() {
+//         $(document).on('click', '.scrap_btn', function() {
+//             alert('엥 못읽냐?');
+//             if(confirm('스크랩 하시겠습니까?')){
+//                 console.log('scrap');
+//                 $(this).children().children().attr('d','M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z')
+//                 $(this).removeClass('scrap_btn');
+//                 $(this).addClass('unscrap_btn');
+//             }else {
+//                 return false;
+//             }
+//         });
+//         $(document).on('click','.unscrap_btn',function () {
+//             $(this).children().children().attr('d','M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z')
+//             console.log('unscrap');
+//             $(this).removeClass('unscrap_btn');
+//             $(this).addClass('scrap_btn');
+//         })
+//     }
+// }
 scrap.init();
-scrap_btn.init();
+//scrap_btn.init();
 
 //스크랩 표시
-function displayScrap(param){
-    let com_num =$(this).data('com-num');
-    if(param.status == 'noScrap'){
-
-    }else{
-        document.getElementById('star').classList.replace('blank', 'checked');
-    }
-
-}
+// function displayScrap(param){
+//     console.log(com_num);
+//     if(param.status == 'noScrap'){
+//
+//     }else{
+//         document.getElementById('star').classList.replace('blank', 'checked');
+//     }
+//
+// }
 
 
 //날짜 남은시간 계산기
--- Coudy/src/main/webapp/WEB-INF/views/company/comHome.jsp
+++ Coudy/src/main/webapp/WEB-INF/views/company/comHome.jsp
@@ -72,7 +72,7 @@
 <%--                             fill="currentColor" class="scrap_star float-end bi bi-star" viewBox="0 0 16 16">--%>
 <%--                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>--%>
 <%--                        </svg>--%>
-                        <span data-com-num="${company.com_num}" style="font-size: 23px; cursor: pointer;" class="scrap_star fa fa-star blank float-end"></span>
+                        <span id="${company.com_num}" data-com-num="${company.com_num}" style="font-size: 23px; cursor: pointer;" class="scrap_star fa fa-star blank float-end"></span>
                         <script>
                             $(function () {
                                 let com_num = ${company.com_num};
@@ -94,6 +94,16 @@
                                 }
                                 selectData(com_num);
                                 console.log(${company.com_num});
+
+                                function displayScrap(param){
+                                    let id=${company.com_num};
+                                    if(param.status == 'noScrap'){
+                                        document.getElementById(id).classList.replace('checked','blank');
+                                    }else{
+                                        document.getElementById(id).classList.replace('blank', 'checked');
+                                    }
+
+                                }
                             });
                         </script>
                     </p>
