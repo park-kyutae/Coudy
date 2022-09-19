844406f09b869fb622c26ecca39138831b5e19c1
dkoqwekl312 <devacc11011@icloud.com>
2022-09-19 22:59:04 +0900

    버그 수정

---
+++ Coudy/src/main/java/kr/spring/study/chat/controller/CreateChatRoomForm.java
@@ -0,0 +1,13 @@
+package kr.spring.study.chat.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CreateChatRoomForm {
+    private String chatName;
+    private String mem_num;
+}
